e06a1bdcdb05b0dea6beeef08024c293c0f20aa0
Ethan W <ethan.wanq@gmail.com>
2023-02-11 13:41:26 -0500

    update

--- public/main.js
+++ public/main.js
@@ -1,25 +1,4 @@
-
-/*const s = "Redirecting in ";
-let t = 5;
-let j;
-let counter = 0;
-let text = document.getElementById("REDIRECT")
-let p = new Promise((res) => {
-    resolve = res;
-});
-let resolve;
-setInterval(()=>{
-
-    counter += 1;
-    t -= Math.trunc(counter/1000);
-    j = s+t;
-    text.innerHTML = j;
-    if(counter == 5000) {
-        res();
-    }
-})
-
-p.then(()=>{
-    window.location.replace("");
-})
-*/
+let a = document.getElementsByClassName("WIX_ADS");
+for(let j = 0; j < a.length; j++) {
+    document.removeChild(a[j]);
+}
